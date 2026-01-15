7f68b22e4b20aa7c13a51712c9f0577ffd39bb6e
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2026-01-15 12:39:05 +0900

    fix: resolve incomaptibility of Rails and Plex

--- config/initializers/phlex.rb
+++ config/initializers/phlex.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# Ensure ActiveSupport's class_attribute is loaded before Phlex
-# This fixes eager loading issues in production where Phlex::Rails::Streaming
-# tries to use class_attribute before ActiveSupport is fully loaded
-require "active_support/core_ext/class/attribute"
-
 module Views
 end
 
@@ -19,3 +14,9 @@ Rails.autoloaders.main.push_dir(
 Rails.autoloaders.main.push_dir(
   Rails.root.join("app/components"), namespace: Components
 )
+
+# FIXME: workaround for https://github.com/yippee-fun/phlex-rails/issues/323
+Phlex::Rails::Loader.tap do |loader|
+  gem_path = Gem.loaded_specs["phlex-rails"].full_gem_path
+  loader.do_not_eager_load(File.join(gem_path, "lib/phlex/rails/streaming.rb"))
+end
